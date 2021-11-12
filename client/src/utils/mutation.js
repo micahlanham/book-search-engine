894aebe1da011ba8967bfa6727bbb80561dc64b4
micahlanham <micahlanham@gmail.com>
2021-11-12 13:39:36 -0500

    mutation file created

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,61 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+    mutation LOGIN_USER($email: String!, $password: String!) {
+        LOGIN_USER(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: SavedBookInput!) {
+    saveBook(book: $book) {
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
